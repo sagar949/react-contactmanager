b317934d41911f3db271db7246670454da04cba0
sagar949 <vidya.pamula@gmail.com>
2018-09-08 23:31:13 +0530

    added Context Api and consumed it

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,47 +1,31 @@
 import React, { Component } from 'react';
 import Contact from './Contact';
+import { Consumer } from '../context';
 
 export default class Contacts extends Component {
-  state = {
-    contacts: [
-      {
-        id: 1,
-        name: 'John Doe',
-        email: 'jdoe@gmail.com',
-        phone: '555-555-5555'
-      },
-      {
-        id: 2,
-        name: 'Vidya Sagar',
-        email: 'vs@gmail.com',
-        phone: '111-111-1111'
-      },
-      {
-        id: 3,
-        name: 'Shiva',
-        email: 'shiva@gmail.com',
-        phone: '333-333-3333'
-      }
-    ]
-  };
-
   deleteContact = id => {
     const { contacts } = this.state;
     const newContacts = contacts.filter(c => c.id !== id);
     this.setState({ contacts: newContacts });
   };
   render() {
-    const { contacts } = this.state;
     return (
-      <React.Fragment>
-        {contacts.map(contact => (
-          <Contact
-            key={contact.id}
-            contact={contact}
-            deleteClickHandler={this.deleteContact.bind(this, contact.id)}
-          />
-        ))}
-      </React.Fragment>
+      <Consumer>
+        {value => {
+          const { contacts } = value;
+          return (
+            <React.Fragment>
+              {contacts.map(contact => (
+                <Contact
+                  key={contact.id}
+                  contact={contact}
+                  deleteClickHandler={this.deleteContact.bind(this, contact.id)}
+                />
+              ))}
+            </React.Fragment>
+          );
+        }}
+      </Consumer>
     );
   }
 }
---
+++ src/context.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+
+const Context = React.createContext();
+
+export class Provider extends Component {
+  state = {
+    contacts: [
+      {
+        id: 1,
+        name: 'John Doe',
+        email: 'jdoe@gmail.com',
+        phone: '555-555-5555'
+      },
+      {
+        id: 2,
+        name: 'Vidya Sagar',
+        email: 'vs@gmail.com',
+        phone: '111-111-1111'
+      },
+      {
+        id: 3,
+        name: 'Shiva',
+        email: 'shiva@gmail.com',
+        phone: '333-333-3333'
+      }
+    ]
+  };
+  render() {
+    return (
+      <Context.Provider value={this.state}>
+        {this.props.children}
+      </Context.Provider>
+    );
+  }
+}
+
+export const Consumer = Context.Consumer;
